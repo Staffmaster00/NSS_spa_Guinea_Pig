3f230614b473c7f029885565acb77741ed2aef7d
Sam Staff <samstaff@Sams-MacBook-Pro.local>
2017-06-20 11:57:52 -0500

    Buttons now work

--- events.js
+++ events.js
@@ -1,7 +1,11 @@
 let articles = document.getElementsByClassName("article-section");
 let lonelyH1 = document.getElementById("page-title");
 let output = document.getElementById("output-target");
-let input = document.getElementById("keypress-input")
+let input = document.getElementById("keypress-input");
+let colorBtn = document.getElementById("add-color");
+let sizeBtn = document.getElementById("make-large");
+let borderBtn = document.getElementById("add-border");
+let roundBtn = document.getElementById("add-rounding");
 for (i = 0; i < articles.length; i++) {
     articles[i].addEventListener("click", function(event) {
         let location = event.target.id;
@@ -17,5 +21,17 @@ lonelyH1.addEventListener("mouseleave", function() {
 console.log("inputValue outside", input.value);
 input.addEventListener("keyup", function() {
     output.innerHTML = input.value
+});
+colorBtn.addEventListener("click", function(){
+	output.classList.toggle("color")
+});
+sizeBtn.addEventListener("click", function(){
+	output.classList.toggle("size")
+});
+borderBtn.addEventListener("click", function(){
+	output.classList.toggle("border")
+});
+roundBtn.addEventListener("click", function(){
+	output.classList.toggle("round")
 });
 
