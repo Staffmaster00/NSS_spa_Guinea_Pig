5d6426c458bf27b9bca1d50fdd3800652b0171c0
Sam Staff <samstaff@Sams-MacBook-Pro.local>
2017-06-20 11:39:53 -0500

    Made output value dynamically equal input from the text area

--- events.js
+++ events.js
@@ -1,19 +1,21 @@
 let articles = document.getElementsByClassName("article-section");
+let lonelyH1 = document.getElementById("page-title");
+let output = document.getElementById("output-target");
+let input = document.getElementById("keypress-input")
 for (i = 0; i < articles.length; i++) {
     articles[i].addEventListener("click", function(event) {
-        console.log("functioning?", event);
-
         let location = event.target.id;
-        
-        document.getElementById("output-target").innerHTML = `You clicked on the ${location} section`;
+        output.innerHTML = `You clicked on the ${location} section`;
     })
 };
-
-let lonelyH1 = document.getElementById("page-title");
-let output = document.getElementById("output-target")
-lonelyH1.addEventListener("mouseenter", function(){
-	output.innerHTML = `You moved your mouse over the header.`
+lonelyH1.addEventListener("mouseenter", function() {
+    output.innerHTML = `You moved your mouse over the header.`
 });
-lonelyH1.addEventListener("mouseleave", function(){
-	output.innerHTML = `You left me!`
-})
+lonelyH1.addEventListener("mouseleave", function() {
+    output.innerHTML = `You left me!`
+});
+console.log("inputValue outside", input.value);
+input.addEventListener("keyup", function() {
+    output.innerHTML = input.value
+});
+
